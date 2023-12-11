4d2b71b358e6a82b1e5b20be95fc271543466ca5
SHEF <Shefqetmush@gmail.com>
2023-12-10 21:11:02 -0500

    adding the x scale and axis

--- src/App.js
+++ src/App.js
@@ -3,33 +3,49 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [gdp, setGdp] = useState([]);
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState("")
 
   const width = 640;
   const height = 400;
-
+  const padding = 60;
+  
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
     )
       .then((res) => res.json())
-      .then((res) => setGdp(res.data));
+      .then((res) => {
+        setData(res.data);
+        setTitle(res.source_name);
+      });
   }, []);
 
   useEffect(() => {
-    if (gdp.length === 0) return;
+    if (data.length === 0) return;
 
     createChart();
-  }, [gdp]);
+  }, [data]);
 
   const createChart = () => {
-      
+      const svg = d3
+              .select("#mySvg")
+              .attr("width", width)
+              .attr("height", height)
+
+      const xScale = d3.scaleTime().domain([data[0], data[data.length - 1][0]])
+
+      svg.append("g")
+          .attr("transform", `translate(0, ${height - padding})`)
+          .call(d3.axisBottom(xScale))
+
   }
 
   return (
     <div className="App">
       <header className="App_inner_Container">
-        <svg id="mySvg" width={width} height={height}></svg>
+        <div className="title">{title}</div>
+        <svg id="mySvg"></svg>
       </header>
     </div>
   );
