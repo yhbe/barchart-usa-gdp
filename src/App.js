022e503cef53da8e07b5ce6ab9ac93f2e6ae7c75
SHEF <Shefqetmush@gmail.com>
2023-12-11 16:55:02 -0500

    appended bars and added tooltip on hover

--- src/App.js
+++ src/App.js
@@ -49,6 +49,58 @@ function App() {
         .attr("transform", `translate(${padding}, 0)`)
         .call(d3.axisLeft(yScale));
 
+      svg
+        .selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => xScale(new Date(d[0])))
+        .attr("y", (d) => yScale(d[1]))
+        .attr("height", (d) => height - padding - yScale(d[1]))
+        .attr("width", (width - 2 * padding) / data.length)
+        .attr("fill", "steelblue")
+        .on("mousemove", (e,d) => {
+          const tooltip = d3.select("#tooltip")
+
+          tooltip.transition()
+                  .duration(200)
+                  .style("opacity", 0.9)
+                  .style("left", e.pageX + 10 + "px")
+                  .style("top", e.pageY + 10 + "px")
+                  .style("font-size", "15px")
+
+          const months = {
+            "01": "Q1",
+            "02": "Q1",
+            "03": "Q1",
+            "04": "Q2",
+            "05": "Q2",
+            "06": "Q2",
+            "07": "Q3",
+            "08": "Q3",
+            "09": "Q3",
+            "10": "Q4",
+            "11": "Q4",
+            "12": "Q4"
+          }
+
+          tooltip
+              .attr("data-date", d[0])
+              .html(`${d[0].split("-")[0]} ${months[d[0].split("-")[1]]} <br/> $${d[1]} Billions`)
+        })
+        .on("mouseout", () => {
+          d3.select("#tooltip").style("opacity", "0")
+        })
+
+        const tooltip = d3.select("body")
+                          .append("div")
+                          .attr("id", "tooltip")
+                          .style("opacity", 0)
+                          .style("position", "absolute")
+                          .style("background-color", "black")
+                          .style("color", "white")
+                          .style("padding", "10px")
   }
 
   return (
